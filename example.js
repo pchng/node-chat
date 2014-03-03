e15688bff08e7177191d1b08b3b3da19f35a1426
Peter Chng <pc6411@gmail.com>
2014-03-03 18:31:47 -0500

    Basic echo client/server.

--- example.js
+++ example.js
@@ -10,7 +10,7 @@ var WebSocketServer = require('ws').Server
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('received: %s', message);
+        ws.send(message);
     });
-    ws.send(message);
 });
 
--- html_client/js/scripts.js
+++ html_client/js/scripts.js
@@ -2,19 +2,34 @@
 (function(application, $, undefined) {
 
   var WEBSOCKET_ADDRESS = "ws://localhost:8088/";
+  var formSelector = "#input";
+  var inputSelector = "#message";
+  var outputSelector = "#output";
   
   var socket;
   
   function init() {
+    setupWebSocketConnection();
   }
   
+  // TODO: PC: How to use Sec-WebSocket-Protocol during handshake?
+  // - Client supplies list of valid sub-protcols and server responds with which one it has chosen.
   function setupWebSocketConnection() {
     if (window.WebSocket) {
-      socket = new WebSocket(webSocketAddress);
+      socket = new WebSocket(WEBSOCKET_ADDRESS);
       socket.onopen = outputTextMessage;
       socket.onmessage = outputTextMessage;
       socket.onclose = outputTextMessage;
       
+      // UI event handlers.
+      $(formSelector).submit(function(e) {
+        e.preventDefault();
+        var message = $(inputSelector).val();
+        
+        if (message) {
+          sendTextMessage(message);
+        }
+      });
       
     } else {
       window.alert("WebSocket is not supported in your browser.");
@@ -22,11 +37,19 @@
   }
   
   function sendTextMessage(message) {
+    if (socket && socket.readyState == WebSocket.OPEN) {
+      socket.send(message);
+    } else {
+      console.error("WebSocket is not open.");
+    }
   }
   
   function outputTextMessage(event) {
     console.log(event);
+    $(outputSelector).append(event.data + "\n");
   }
+  
+  init();
   
 
 })(window.application = window.application || {}, jQuery);
