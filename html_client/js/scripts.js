e95588f80cfb2300ff46bb144d66f77d1e3e1d46
Peter Chng <pc6411@gmail.com>
2014-03-06 00:56:21 -0500

    Basic retry.

--- html_client/js/scripts.js
+++ html_client/js/scripts.js
@@ -3,7 +3,6 @@
 
   var config = window.config;
 
-  // TODO: PC: Verify configuration present!
   if (!config) {
     console.error("No configuration present.");
     return;
@@ -12,26 +11,23 @@
   var formSelector = "#input";
   var inputSelector = "#message";
   var outputSelector = "#output";
+  var closeSelector = "#close";
   
   var socket;
   var retryQueue = [];
-  var retryIntervalId;
+  var retryPendingId;
   var SIMPLE_RETRY_INTERVAL = 5000;
   
   function init() {
     setupWebSocketConnection();
     attachEventHandlers();
-
-    // TODO: PC: May want to use event-triggered setTimeout() instead of interval.
-    // - Only re-call setTimeout() if more on queue; initiate when putting onto the queue if intervalId not set?
-    // retryIntervalId = window.setInterval(checkRetryQueue, SIMPLE_RETRY_INTERVAL);
   }
   
   // NOTE: To use Sec-WebSocket-Protocol during handshake:
   // - Client supplies list of valid sub-protcols and server responds with which one it has chosen.
   // new WebSocket(address, (sub)proctocol(s));
   // - subprotocols can be a string or an array of strings.
-  function setupWebSocketConnection(callback) {
+  function setupWebSocketConnection(successCallback, errorCallback) {
     if (!window.WebSocket) {
       window.alert("WebSocket is not supported in your browser.");
       return;
@@ -40,17 +36,26 @@
     socket = new WebSocket(config.WEBSOCKET_ADDRESS);
     socket.onopen = function(event) {
       console.log(event);
-      if (callback) {
-        callback(event);
+      if (successCallback) {
+        successCallback(event);
       }
-    };
-    socket.onmessage = function(event) {
-      console.log(event);
-      outputTextMessage(event.data);
     };
     socket.onclose = function(event) {
       console.log(event);
+      // Anything other than 1000 is not a normal close event.
+      if (event && event.code != 1000 && errorCallback) {
+        errorCallback(event);
+      }
       outputTextMessage("Connection closed");
+    };
+    // NOTE: This doesn't seem to fire.
+    socket.onerror = function(event) {
+      console.log(event);
+    }
+
+    socket.onmessage = function(event) {
+      console.log(event);
+      outputTextMessage(event.data);
     };
   }
 
@@ -64,6 +69,12 @@
         sendTextMessage(message);
       }
     });
+
+    $(closeSelector).click(function(e) {
+      if (socket) {
+        socket.close();
+      }
+    });
   }
   
   function sendTextMessage(message) {
@@ -73,21 +84,28 @@
       return;
     }
 
-    // TODO: PC: Really two problems, somewhat separate, don't confuse them:
-    // 1) Retry: Assumes problem was transient and can be fixed.
-    // 2) Reconnect: Should NOT be done periodically but rather on demand.
-    // - If couldn't reconnect, add to retry.
-
-
     if (socket.readyState != WebSocket.OPEN) {
       retryQueue.push(message);
       console.log("Socket status was %s. Putting message on retry queue. Queue size: %s", socket.readyState, retryQueue.length);
 
       // NOTE: This behaviour triggers a retry immediately (typically on user action) and the 
       // periodic retry action will only take place in the absence of this trigger.
-      // NOTE: No need to clear retryIntervalId if doing this!
-      window.clearTimeout(retryIntervalId);
-      retryIntervalId = window.setTimeout(checkRetryQueue, 0);
+      // NOTE: No need to clear retryPendingId if doing this!
+
+      // TODO: PC: Fix this. Seems to cause too many setTimeout events to queue up if
+      // sending triggered too fast. Race condition?
+      // 1) Timeout is set and then another timeout is set.
+      // 2) Clear timeout fires, but only clears the SECOND timeout; the first remains in play.
+      // - Only most recent stored.
+      // 3) Multiple setTimeout() calls but only one clearTimeout() call so many remain in play...
+      // 4) May need an array - add pendingId to array, clear all when clearing timeout.
+      // - Maybe we SHOULD wait for the next retry period? Make this configurable by putting
+      // this code into a function that accepts a boolean parameter, tryImmediately.
+      if (!retryPendingId) {
+        // NOTE: This works.
+        // window.clearTimeout(retryPendingId);
+        retryPendingId = window.setTimeout(checkRetryQueue, 100);
+      }
     } else {
       socket.send(message);
     }
@@ -95,27 +113,22 @@
 
   // TODO: PC: How many messages to try at a time? Batch size? Stop after failure if batch?
   function checkRetryQueue() {
+    // NOTE: When this function is invoked from a JS Timer, it will BLOCK until it has 
+    // completed execution, just as if was called normally. (Single-threaded)
+
+    // TODO: PC: Because the execution of this function blocks, can probably clear
+    // retryPendingId here unconditionally.
+
     console.debug("Checking retry queue at %s.", new Date());
     if (retryQueue.length <= 0) {
-      console.debug("Retry queue was empty.");
+      console.debug("Retry queue was empty: Take no action.");
       // Clear the retry ID to indicate no pending retry actions.
-      retryIntervalId = null;
+      retryPendingId = null;
       return;
     }
 
     if (socket.readyState != WebSocket.OPEN) {
-      // TODO: PC: Isn't working here.
-      // If fails to connect, then retryIntervalId stays set and setTimeout() isn't called again.
-      // Need to take separate action of fails: setTimeout() and try again.
-      // - Think about how to refactor this!
-      // - Also: When to clear intervalId? Can't at beginning, as even though JS is single-threaded,
-      // code may "interleave" so if cleared at the beginning, another call to sendTextMessage()
-      // could cause setTimeout() to be invoked BEFORE this function returns. (?Or is the 
-      // execution of this function contiguous and NOT interleaved? Test!)
-      // NEGATIVE: Results WILL NOT interleave, so can INDEED clear intervalId at the beginning of
-      // this method!
-
-      setupWebSocketConnection(retryMessage);
+      setupWebSocketConnection(retryMessage, failedToConnect);
     } else {
       retryMessage();
     }
@@ -129,12 +142,17 @@
       }
 
       if (retryQueue.length > 0) {
-        // Try later if queue is not empty.
-        retryIntervalId = window.setTimeout(checkRetryQueue, SIMPLE_RETRY_INTERVAL);
+        // Couldn't empty out the queue, so try again later.
+        retryPendingId = window.setTimeout(checkRetryQueue, SIMPLE_RETRY_INTERVAL);
       } else {
         // Clear the retry ID to indicate no pending retry actions.
-        retryIntervalId = null;
+        retryPendingId = null;
       }
+    }
+
+    function failedToConnect(event) {
+      // Failed to establish connection. Wait to try again.
+      retryPendingId = window.setTimeout(checkRetryQueue, SIMPLE_RETRY_INTERVAL);
     }
   }
   
